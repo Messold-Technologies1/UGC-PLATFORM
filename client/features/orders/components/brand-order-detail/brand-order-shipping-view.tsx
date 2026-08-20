003a5129144b21f35518017c668966405e209fa0
beasroy <roybipasha676@gmail.com>
2026-08-20 18:00:33 +0530

    fixing build error

--- client/features/orders/components/brand-order-detail/brand-order-shipping-view.tsx
+++ client/features/orders/components/brand-order-detail/brand-order-shipping-view.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from "react";
 import Link from "next/link";
 import { AlertCircle, ArrowLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
